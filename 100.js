22dafda149adc923ead2ce90b38924ec79d99950
alilangah78 <125276533+alilangah78@users.noreply.github.com>
2024-05-08 01:07:22 +0500

    Add files via upload

---
+++ 100.js
@@ -0,0 +1,10 @@
+let personName = "ERIC";
+console.log(`hellow ${personName}, would you like to learn typescript`);
+let name = "ERIC";
+console.log("lowercase:", name.toLowerCase());
+console.log("uppercase :", name.toUpperCase());
+console.log("tittlecase :", name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
+const qoute = "A persone  who newer made a mistake  newer tried  anything new";
+const Author = "Albert Einstein";
+console.log(`${Author} once said, ${qoute}`);
+export {};
---
+++ 100.ts
@@ -0,0 +1,11 @@
+let personName : string = "ERIC";
+console.log(`hellow ${personName}, would you like to learn typescript`);
+
+let name :string = "ERIC";
+console.log("lowercase:" ,name.toLowerCase());
+console.log("uppercase :", name.toUpperCase());
+console.log("tittlecase :", name.charAt(0).toUpperCase() +name.slice(1).toLowerCase());
+
+const qoute :string = "A persone  who newer made a mistake  newer tried  anything new";
+const Author :string ="Albert Einstein";
+console.log(`${Author} once said, ${qoute}`);
